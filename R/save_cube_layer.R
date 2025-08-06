cf0bb3b5993e7eacba701f586a801214603e1f52
Acanthiza <nigel.willoughby@sa.gov.au>
2025-08-06 11:29:21 +0930

    remove gdalcubes_options as they appear to do nothing

--- R/save_cube_layer.R
+++ R/save_cube_layer.R
@@ -8,23 +8,11 @@ save_cube_layer <- function(items
                             , cloud_mask = NULL
                             , base_dir
                             , settings
-                            , gdalcubes_config = list(VSI_CACHE = "TRUE"
-                                                      , GDAL_CACHEMAX = "3%"
-                                                      , VSI_CACHE_SIZE = "100000000"
-                                                      , GDAL_HTTP_MULTIPLEX = "YES"
-                                                      , GDAL_INGESTED_BYTES_AT_OPEN = "32000"
-                                                      , GDAL_DISABLE_READDIR_ON_OPEN = "EMPTY_DIR"
-                                                      , GDAL_HTTP_VERSION = "2"
-                                                      , GDAL_HTTP_MERGE_CONSECUTIVE_RANGES = "YES"
-                                                      , GDAL_NUM_THREADS = parallel::detectCores() * 3 / 4
-                                                      )
+                            , cores = parallel::detectCores() * 3 / 4
                             , ...
                             ) {
   
-  # gdalcubes config --------
-  purrr::iwalk(gdalcubes_config
-               , \(x, idx) gdalcubes::gdalcubes_set_gdal_config(as.character(idx), as.character(x))
-               )
+  gdalcubes::gdalcubes_options(parallel = cores)
   
   layer_to_get <- items |>
     rstac::items_assets() |>
