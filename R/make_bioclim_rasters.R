dew-landscapes/envRas

commit a41feadbad65e54afd6090079817af081d4be34d

updates: bimod; tar_file_read; make_cube_dir; numbering; explore tifs; predits::bcvars instead of bioclima::clima; distance done by tiles; more tc

R/make_bioclim_rasters.R
@@ -4,20 +4,20 @@ make_bioclim_rasters <- function(files_df
                                  , force_new = TRUE
                                  ) {
   
-  bc <- bioclima::clima(bios = c(1:19)
-                        , tmin = terra::rast(files_df$files[files_df$layer == "tmin"][[1]]$path)
-                        , tmax = terra::rast(files_df$files[files_df$layer == "tmax"][[1]]$path)
-                        , tavg = terra::rast(files_df$files[files_df$layer == "tavg"][[1]]$path)
-                        , prcp = terra::rast(files_df$files[files_df$layer == "rain"][[1]]$path)
-                        )
+  bc <- predicts::bcvars(prec = terra::rast(files_df$files[files_df$layer == "rain"][[1]]$path)
+                         , tmin = terra::rast(files_df$files[files_df$layer == "tmin"][[1]]$path)
+                         , tmax = terra::rast(files_df$files[files_df$layer == "tmax"][[1]]$path)
+                         )
   
   purrr::map(names(bc)
              , \(x) {
                
+               use_x <- paste0("bio", stringr::str_pad(readr::parse_number(x), width = 2, pad = 0))
+               
                out_file <- fs::path(out_dir
-                                    , paste0(x
+                                    , paste0(use_x
                                              , "__"
-                                             , x
+                                             , use_x
                                              , "__"
                                              , start_date
                                              , ".tif"

010_setup.R
@@ -25,15 +25,10 @@ list(
   # targets --------
   ## settings-------
   ### setup -------
-  tar_target(name = set_file
-               , command = fs::path("settings/setup.yaml")
-               , format = "file"
-               , deployment = "main"
-               )
-  , tar_target(name = settings
-               , command = yaml::read_yaml(set_file)
-               , deployment = "main"
-               )
+  tar_file_read(settings
+                , "settings/setup.yaml"
+                , yaml::read_yaml(!!.x)
+                )
   ## extent directory -------
   , tar_target(extent_dir
                , envFunc::name_env_out(set_list = list(extent = settings$extent)
@@ -44,7 +39,7 @@ list(
   ## maps -------
   ### extent sf -------
   , tar_target(name = extent_sf_file
-               , command = fs::path("..", "..", "..", "data", "vector", paste0(settings$extent$vector, ".parquet"))
+               , command = fs::path(settings$data_dir, "vector", paste0(settings$extent$vector, ".parquet"))
                , format = "file"
                )
   , tar_target(name = extent_sf

020_satellite.R
@@ -11,6 +11,7 @@ tars <- yaml::read_yaml("_targets.yaml")
 # source ------
 tar_source(c("R/save_satellite_layer.R"
              , "R/make_indice.R"
+             , "R/make_cube_dir.R"
              )
            )
 
@@ -22,47 +23,26 @@ targets <- list(
   # targets --------
   ## settings -------
   ### setup -------
-  tar_target(name = set_file
-               , command = fs::path("settings/setup.yaml")
-               , format = "file"
-               )
-  , tar_target(name = settings
-               , command = yaml::read_yaml(set_file)
-               )
+  tar_file_read(settings
+                , "settings/setup.yaml"
+                , yaml::read_yaml(!!.x)
+                )
   ### satellite ------
-  , tar_target(set_file_satellite
-               , fs::path("settings/satellite.yaml")
-               , format = "file"
-               )
-  , tar_target(settings_satellite
-               , yaml::read_yaml(set_file_satellite)
-               )
+  , tar_file_read(settings_satellite
+                  , "settings/satellite.yaml"
+                  , yaml::read_yaml(!!.x)
+                  )
   ## external objects ------
-  , tar_target(extent_sf_file
-               , fs::path(tars$setup$store, "objects", "extent_sf")
-               , format = "file"
-               )
-  , tar_target(extent_sf
-               , readRDS(extent_sf_file)
-               )
+  , tar_file_read(extent_sf
+                  , fs::path(tars$setup$store, "objects", "extent_sf")
+                  , readRDS(!!.x)
+                  )
   ## cube directory ------
   , tar_target(cube_directory
-               , name_env_tif(x = c(settings$extent
-                                    , settings$grain
-                                    , source = settings_satellite$source
-                                    , collection = settings_satellite$collection
-                                    )
-                              , context_defn = c("vector", "filt_col", "filt_level", "buffer")
-                              , cube_defn = c("temp", "res")
-                              , dir_only = TRUE
-                              , prefixes = c("sat", "use")
-                              , fill_null = TRUE
-                              )$out_dir %>%
-                 fs::path("I:", .)
-               )
-  ## make cube directory --------
-  , tar_target(make_cube_dir
-               , fs::dir_create(cube_directory)
+               , make_cube_dir(set_scale = settings
+                               , set_source = settings_satellite
+                               )
+               , format = "file"
                )
   ### base grid -------
   , tar_target(base_grid_path
@@ -70,7 +50,7 @@ targets <- list(
                                 , out_res = settings$grain$res
                                 , out_epsg = settings$crs$proj
                                 , use_mask = extent_sf
-                                , out_file = fs::path(dirname(make_cube_dir), "base.tif")
+                                , out_file = fs::path(dirname(cube_directory), "base.tif")
                                 , overwrite = TRUE
                                 , ret = "path"
                                 , datatype = "INT1U"

030_climate.R
@@ -11,6 +11,7 @@ tars <- yaml::read_yaml("_targets.yaml")
 tar_source(c("R/download_nc.R"
              , "R/make_bioclim_rasters.R"
              , "R/align_ras.R"
+             , "R/make_cube_dir.R"
              )
            )
 
@@ -29,47 +30,26 @@ targets <- list(
   # targets --------
   ## settings -------
   ### setup -------
-  tar_target(name = set_file
-               , command = fs::path("settings/setup.yaml")
-               , format = "file"
-               )
-  , tar_target(name = settings
-               , command = yaml::read_yaml(set_file)
-               )
+  tar_file_read(settings
+                , "settings/setup.yaml"
+                , yaml::read_yaml(!!.x)
+                )
   ### climate ------
-  , tar_target(set_file_climate
-               , fs::path("settings/climate.yaml")
-               , format = "file"
-               )
-  , tar_target(settings_climate
-               , yaml::read_yaml(set_file_climate)
-               )
+  , tar_file_read(settings_climate
+                  , "settings/climate.yaml"
+                  , yaml::read_yaml(!!.x)
+                  )
   ## external objects ------
-  , tar_target(extent_sf_file
-               , fs::path(tars$setup$store, "objects", "extent_sf")
-               , format = "file"
-               )
-  , tar_target(extent_sf
-               , readRDS(extent_sf_file)
-               )
+  , tar_file_read(extent_sf
+                  , fs::path(tars$setup$store, "objects", "extent_sf")
+                  , readRDS(!!.x)
+                  )
   ## cube directory ------
   , tar_target(cube_directory
-               , name_env_tif(x = c(settings$extent
-                                    , settings_climate$grain
-                                    , source = settings_climate$source
-                                    , collection = settings_climate$collection
-                                    )
-                              , context_defn = c("vector", "filt_col", "filt_level", "buffer")
-                              , cube_defn = c("temp", "res")
-                              , dir_only = TRUE
-                              , prefixes = c("sat", "use")
-                              , fill_null = TRUE
-                              )$out_dir %>%
-                 fs::path("I:", .)
-               )
-  ### make cube directory --------
-  , tar_target(make_cube_dir
-               , fs::dir_create(cube_directory)
+               , make_cube_dir(set_scale = settings
+                               , set_source = settings_climate
+                               )
+               , format = "file"
                )
   ### base grid -------
   # Not sure this is necessary (could use extent_sf in bbox instead) but will invalidate the downloads if it is removed

040_coast.R
@@ -3,94 +3,78 @@ library(targets)
 library(tarchetypes)
 library(geotargets)
 library(crew)
-library(crew.cluster)
 
 # tars -------
 tars <- yaml::read_yaml("_targets.yaml")
 
 # source ------
-tar_source(c("R/make_dist_rast.R"
+tar_source(c("R/make_dist_tile.R"
              , "R/terra_reproject.R"
-             , "R/make_coast_raster.R"
+             , "R/make_dist_raster.R"
+             , "R/make_cube_dir.R"
              )
            )
 
-# cores --------
-use_cores <- floor(parallel::detectCores() * 3 / 4)
+## cores --------
+possible_cores <- parallel::detectCores() * 2 / 3
+max_cores <- 100
+use_cores <- min(possible_cores, max_cores)
 
-# ram -------
+## RAM -------
 total_terra_ram_prop <- 0.6 # across all cores
-terra_memfrac <- total_terra_ram_prop / use_cores # prop of available memory allowed per core (or per tile)
+terra_memfrac <- total_terra_ram_prop / use_cores  # prop of available memory allowed per core (or per SDM)
 
+# controllers  ---------
+this_run_start <- format(Sys.time(), "%Y%m%d_%H%M")
+
+main_controller <- crew_controller_local(workers = use_cores
+                                         , options_local = crew_options_local(log_directory = fs::path(tars$coast$store
+                                                                                                       , "log"
+                                                                                                       , this_run_start
+                                                                                                       , "main_controller"
+                                                                                                       )
+                                                                              )
+                                         , name = "main_controller"
+                                         , tasks_max = 1
+                                         )
 # tar options --------
-tar_option_set(packages = sort(unique(yaml::read_yaml("settings/packages.yaml")$packages))
-               , controller = crew_controller_local(workers = use_cores
-                                                    , crashes_max = 0L
-                                                    , options_local = crew_options_local(log_directory = fs::path(tars$distance_to$store, "log")
-                                                                                         , log_join = TRUE
-                                                                                         )
-                                                    )
+tar_option_set(packages = sort(unique(yaml::read_yaml("settings/packages.yaml")$coast))
+               , controller = main_controller
                )
 
 # targets --------
 targets <- list(
   ## settings -------
   ### setup -------
-  tar_target(name = set_file
-               , command = fs::path("settings/setup.yaml")
-               , format = "file"
-               )
-  , tar_target(name = settings
-               , command = yaml::read_yaml(set_file)
-               )
-  ### distance_to -------
-  , tar_target(name = set_file_distance_to
-               , command = fs::path("settings/distance_to.yaml")
-               , format = "file"
-               )
-  , tar_target(name = settings_distance_to
-               , command = yaml::read_yaml(set_file_distance_to)
-               )
+  tar_file_read(settings
+                , "settings/setup.yaml"
+                , yaml::read_yaml(!!.x)
+                )
+  ### coast -------
+  , tar_file_read(settings_coast
+                  , "settings/coast.yaml"
+                  , yaml::read_yaml(!!.x)
+                  )
   ## cube directory ------
   , tar_target(cube_directory
-               , name_env_tif(x = c(settings$extent
-                                    , settings$grain
-                                    , source = settings_distance_to$source
-                                    , collection = settings_distance_to$collection
-                                    )
-                              , context_defn = c("vector", "filt_col", "filt_level", "buffer")
-                              , cube_defn = c("temp", "res")
-                              , dir_only = TRUE
-                              , prefixes = c("sat", "use")
-                              , fill_null = TRUE
-                              )$out_dir %>%
-                 fs::path("I:", .)
-               )
-  ## make cube directory --------
-  , tar_target(make_cube_dir
-               , fs::dir_create(cube_directory)
-               )
-  ## maps -------
-  ### coast ------
-  , tar_target(coast_file
-               , fs::path("..", "..", "..", "data", "vector", "aus.parquet")
+               , make_cube_dir(set_scale = settings
+                               , set_source = settings_coast
+                               )
                , format = "file"
                )
-  , tar_target(coast_sf
-               , arrow::open_dataset(coast_file) |>
-                 sfarrow::read_sf_dataset() |>
-                 sf::st_transform(crs = sf::st_crs(terra::rast(base_grid_path))) |>
-                 sf::st_make_valid()
-               )
-  , tar_target(coast_sf_line
-               , coast_sf |>
-                 sf::st_cast("MULTILINESTRING") |>
-                 sf::st_cast("LINESTRING")
-               )
-  ### base grid -------
+  ### base grid path-------
   , tar_target(base_grid_path
                , fs::path(dirname(cube_directory), "base.tif")
+               , format = "file"
                )
+  ## maps --------
+  ### coast ------
+  , tar_target(coast_file
+               , fs::path(settings$data_dir, "vector", "aus.parquet")
+               , format = "file"
+               )
+  ### coast mask --------
+  # no separate mask needed as the coast is the mask
   ## prep -------
   ### dates -------
   , tar_target(name = max_date
@@ -99,7 +83,7 @@ targets <- list(
   , tar_target(name = min_date
                , command = lubridate::as_date(max_date) - lubridate::as.period(envFunc::find_name(settings, "temp")) + lubridate::as.period("P1D")
                )
-  ### files --------
+  ### out file --------
   , tar_target(coast_tif_file
                  , fs::path(cube_directory
                             , paste0("coast__distance__"
@@ -114,42 +98,43 @@ targets <- list(
 if(yaml::read_yaml("settings/setup.yaml")$grain$res == 90) {
   
   distance <- list(
-    ## split -------
+    ## coast--------
+    ### split -------
     tar_target(name = tile_extents
-               , make_tile_extents(base_grid_path = base_grid_path)
+               , envTargets::make_tile_extents(base_grid_path = base_grid_path)
                )
     
     , tar_target(use_memfrac
                  , if(nrow(tile_extents >= use_cores)) {terra_memfrac} else
                    {(total_ram * total_terra_ram_prop / nrow(tile_extents)) / total_ram}
                  )
-    ## coast--------
     ### apply -------
-    , tar_terra_rast(tile_coast
-                     , make_dist_rast(base_grid_path
-                                      , tile_extents
-                                      , sf_line = coast_sf_line
-                                      , terra_options = list(memfrac = use_memfrac)
-                                      , datatype = "INT4S" # integer metre accuracy
-                                      )
-                     , pattern = map(tile_extents)
-                     , cue = tar_cue(mode = "never")
-                     )
+    , tar_target(tile_coast
+                 , make_dist_tile(base_grid_path = base_grid_path
+                                  , extent = tile_extents
+                                  , sf_dist_file = coast_file
+                                  , terra_options = list(memfrac = use_memfrac)
+                                  , sf_mask_file = coast_file # = coast_mask_file
+                                  , sf_mask_positive = TRUE
+                                  , dist_limit = 5000
+                                  , out_dir = fs::path(tars$coast$store, "tiles")
+                                  # via dots... to terra::lapp
+                                  , wopt = list(datatype = "INT2S") # easily encompasses -10000 to 10000 m
+                                  )
+                 , pattern = map(tile_extents)
+                 , format = "file"
+                 )
     ### combine -------
     , tar_target(coast
-                 , make_coast_raster(tiles = tile_coast
-                                     , base_grid_path = base_grid_path
-                                     , coast_sf = coast_sf
-                                     , out_file = coast_tif_file
-                                     # passed via ... to terra::lapp()
-                                     , overwrite = TRUE
-                                     , wopt = list(datatype = "INT4S"
-                                                   , gdal = c("TILED=YES"
-                                                            , "COPY_SRC_OVERVIEWS=YES"
-                                                            , "COMPRESS=DEFLATE"
-                                                            )
-                                                   )
-                                     )
+                 , make_dist_raster(tile_coast
+                                    , out_file = coast_tif_file
+                                    , datatype = "INT2S" # easily encompasses -10000 to 10000 m
+                                    , gdal = c("TILED=YES"
+                                               , "COPY_SRC_OVERVIEWS=YES"
+                                               , "COMPRESS=DEFLATE"
+                                               )
+                                    , names = "distance"
+                                    )
                  , format = "file"
                  )
     )
@@ -170,7 +155,7 @@ if(yaml::read_yaml("settings/setup.yaml")$grain$res < 90) {
                                    , base_grid_path = base_grid_path
                                    , out_file = coast_tif_file
                                    , method = "bilinear"
-                                   , datatype = "INT4S"
+                                   , datatype = "INT2S"
                                    , overwrite = TRUE
                                    )
                  )

050_watercourse.R
@@ -12,6 +12,7 @@ tars <- yaml::read_yaml("_targets.yaml")
 tar_source(c("R/make_dist_tile.R"
              , "R/terra_reproject.R"
              , "R/make_dist_raster.R"
+             , "R/make_cube_dir.R"
              )
            )
 
@@ -57,40 +58,25 @@ targets <- list(
                   )
   ## cube directory ------
   , tar_target(cube_directory
-               , fs::path("/mnt"
-                          , "envshare" # Hack while no write access to SAMBA. This should be "envcube"
-                          , envRaster::name_env_tif(x = c(settings$extent
-                                               , settings$grain
-                                               , source = settings_wc$source
-                                               , collection = settings_wc$collection
-                                               )
-                                         , context_defn = c("vector", "filt_col", "filt_level", "buffer")
-                                         , cube_defn = c("temp", "res")
-                                         , dir_only = TRUE
-                                         , prefixes = c("sat", "use")
-                                         , fill_null = TRUE
-                                         )$out_dir
-                          )
-               )
-  ## wc mask --------
-  , tar_target(wc_mask_file
-               , fs::path(settings$data_dir, "vector", "water_poly.parquet")
+               , make_cube_dir(set_scale = settings
+                               , set_source = settings_wc
+                               )
                , format = "file"
                )
-  ## base grid path-------
+  ### base grid path-------
   , tar_target(base_grid_path
                , fs::path(dirname(cube_directory), "base.tif")
-               , format = "file"
-               )
-  ## make cube directory --------
-  , tar_target(make_cube_dir
-               , fs::dir_create(cube_directory)
                , format = "file"
                )
   ## maps -------
   ### wc ------
   , tar_target(wc_file
                , fs::path(settings$data_dir, "vector", "water_lines.parquet")
+               , format = "file"
+               )
+  ### wc mask --------
+  , tar_target(wc_mask_file
+               , fs::path(settings$data_dir, "vector", "water_poly.parquet")
                , format = "file"
                )
   ## prep -------
@@ -101,7 +87,7 @@ targets <- list(
   , tar_target(name = min_date
                , command = lubridate::as_date(max_date) - lubridate::as.period(envFunc::find_name(settings, "temp")) + lubridate::as.period("P1D")
                )
-  ### files --------
+  ### out file --------
   , tar_target(wc_tif_file
                  , fs::path(cube_directory
                             , paste0("watercourse__distance__"
@@ -130,9 +116,10 @@ if(yaml::read_yaml("settings/setup.yaml")$grain$res == 90) {
     , tar_target(tile_wc
                  , make_dist_tile(base_grid_path = base_grid_path
                                   , extent = tile_extents
-                                  , vect_line_file = wc_file
+                                  , sf_dist_file = wc_file
                                   , terra_options = list(memfrac = use_memfrac)
-                                  , vect_mask_file = wc_mask_file
+                                  , sf_mask_file = wc_mask_file
+                                  , sf_mask_positive = FALSE
                                   , dist_limit = 2000
                                   , out_dir = fs::path(tars$watercourse$store, "tiles")
                                   # via dots... to terra::lapp

060_tc.R
@@ -13,6 +13,7 @@ tars <- yaml::read_yaml("_targets.yaml")
 # source ------
 tar_source(c("R/save_satellite_layer.R"
              , "R/make_indice.R"
+             , "R/make_cube_dir.R"
              )
            )
 
@@ -24,51 +25,31 @@ targets <- list(
   # targets --------
   ## settings -------
   ### setup -------
-  tar_target(name = set_file
-               , command = fs::path("settings/setup.yaml")
-               , format = "file"
-               )
-  , tar_target(name = settings
-               , command = yaml::read_yaml(set_file)
-               )
+  tar_file_read(settings
+                , "settings/setup.yaml"
+                , yaml::read_yaml(!!.x)
+                )
   ### tc ------
-  , tar_target(set_file_tc
-               , fs::path("settings/tc.yaml")
-               , format = "file"
-               )
-  , tar_target(settings_tc
-               , yaml::read_yaml(set_file_tc)
-               )
+  , tar_file_read(settings_tc
+                  , "settings/tc.yaml"
+                  , yaml::read_yaml(!!.x)
+                  )
   ## external objects ------
-  , tar_target(extent_sf_file
-               , fs::path(tars$setup$store, "objects", "extent_sf")
-               , format = "file"
-               )
-  , tar_target(extent_sf
-               , readRDS(extent_sf_file)
-               )
+  , tar_file_read(extent_sf
+                  , fs::path(tars$setup$store, "objects", "extent_sf")
+                  , readRDS(!!.x)
+                  )
   ## cube directory ------
   , tar_target(cube_directory
-               , name_env_tif(x = c(settings$extent
-                                    , settings$grain
-                                    , source = settings_tc$source
-                                    , collection = settings_tc$collection
-                                    )
-                              , context_defn = c("vector", "filt_col", "filt_level", "buffer")
-                              , cube_defn = c("temp", "res")
-                              , dir_only = TRUE
-                              , prefixes = c("sat", "use")
-                              , fill_null = TRUE
-                              )$out_dir %>%
-                 fs::path("I:", .)
-               )
-  ## make cube directory --------
-  , tar_target(make_cube_dir
-               , fs::dir_create(cube_directory)
+               , make_cube_dir(set_scale = settings
+                               , set_source = settings_tc
+                               )
+               , format = "file"
                )
   ### base grid -------
   , tar_target(base_grid_path
                , fs::path(dirname(cube_directory), "base.tif")
+               , format = "file"
                )
   ## prep -------
   ### dates -------

070_soil.R
@@ -13,6 +13,7 @@ tar_source(c("R/make_dist_rast.R"
              , "R/terra_reproject.R"
              , "R/make_coast_raster.R"
              , "R/save_soil_layer.R"
+             , "R/make_cube_dir.R"
              )
            )
 
@@ -41,28 +42,16 @@ targets <- list(
                   )
   ## cube directory ------
   , tar_target(cube_directory
-               , name_env_tif(x = c(settings$extent
-                                    , list(temp = settings_soil$grain$temp, res = settings$grain$res)
-                                    , source = settings_soil$source
-                                    , collection = settings_soil$collection
-                                    )
-                              , context_defn = c("vector", "filt_col", "filt_level", "buffer")
-                              , cube_defn = c("temp", "res")
-                              , dir_only = TRUE
-                              , prefixes = c("sat", "use")
-                              , fill_null = TRUE
-                              )$out_dir %>%
-                 fs::path("I:", .)
-               )
-  ## make cube directory --------
-  , tar_target(make_cube_dir
-               , fs::dir_create(cube_directory)
+               , make_cube_dir(set_scale = settings
+                               , set_source = settings_soil
+                               )
+               , format = "file"
                )
   ### base grid -------
-  , tar_file_read(base_grid_path
-                  , fs::path(tars$satellite$store, "objects", "cube_directory")
-                  , fs::path(dirname(readRDS(!!.x)), "base.tif")
-                  )
+  , tar_target(base_grid_path
+               , fs::path(dirname(cube_directory), "base.tif")
+               , format = "file"
+               )
   ## download cube -------
   ## prep -------
   ### dates -------

commit ac67da6d1970da91761a1ae64ff7a525bdfb7558

fix bug introduced in a41fead (due to change to predicts::bcvars). Use 'bioclim' as the method for bioclim tifs

R/make_bioclim_rasters.R
@@ -16,9 +16,7 @@ make_bioclim_rasters <- function(files_df
                
                out_file <- fs::path(out_dir
                                     , paste0(use_x
-                                             , "__"
-                                             , use_x
-                                             , "__"
+                                             , "__bioclim__"
                                              , start_date
                                              , ".tif"
                                              )
@@ -26,7 +24,7 @@ make_bioclim_rasters <- function(files_df
                
                if(any(!file.exists(out_file), force_new)) {
                
-                 terra::writeRaster(bc[x]
+                 terra::writeRaster(bc[[x]]
                                     , filename = out_file
                                     , overwrite = TRUE
                                     )